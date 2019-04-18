7fcf690909e0b70dcd3a53e29e0d8437754317e3
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-04-18 19:25:12 +0200

    added shop model

--- app/models/bshop.rb
+++
@@ -1,2 +0,0 @@
-class Bshop < ApplicationRecord
-end
--- db/migrate/20190418172101_create_bshops.rb
+++
@@ -1,8 +0,0 @@
-class CreateBshops < ActiveRecord::Migration[5.2]
-  def change
-    create_table :bshops do |t|
-
-      t.timestamps
-    end
-  end
-end
